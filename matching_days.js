6e4484171b900b70c42f7e74c5d85d6cca90e0f4
sivuyisiwe <sivuyisiwequse0@gmail.com>
2017-03-22 11:36:31 +0200

    space changes

--- matching_days.js
+++ matching_days.js
@@ -4,9 +4,9 @@ var selectAllDays = document.querySelectorAll('.weekday')
 
 function Datefunction(){
   for(var i=0; i < Alldays.length; i++){
-    selectAllDays[i].classList.remove('colour1')
-    selectAllDays[i].classList.remove('colour2')
-    selectAllDays[i].classList.remove('sameDay')
+  selectAllDays[i].classList.remove('colour1')
+  selectAllDays[i].classList.remove('colour2')
+  selectAllDays[i].classList.remove('sameDay')
 
   var selectedDate = document.getElementById('fstDate').value;
   var inputval = new Date(selectedDate)
@@ -15,13 +15,13 @@ function Datefunction(){
   var secondSelection = document.getElementById('sndDate').value;
   var inputTwo = new Date(secondSelection)
   var newDate2 = inputTwo.getDay()
-  
+
 if(newDate === newDate2){
   selectAllDays[newDate2].classList.add('sameDay')
 }
 else {
   selectAllDays[newDate].classList.add('colour1')
   selectAllDays[newDate2].classList.add('colour2')
-}
-}
+    }
+  }
 }
